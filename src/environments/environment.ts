fa7677dbcb586a7fb2b6967fab20c959fcf389a8
Omar-Ghon <o.ghoneim04@gmail.com>
2026-03-09 12:19:16 -0400

    Updating web app url after migration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  lovebugApiUrl: 'https://script.google.com/macros/s/AKfycbxZlW1EWHcr87JMdUsL29yXbS35eK1pEJezpTXF_L2asMR5CHFd1loX3nDgk1f-YbJS/exec'
+  lovebugApiUrl: 'https://script.google.com/macros/s/AKfycbw8z9eJN9aqJMhTwUdGkmV-6OzF0rGY0sNFWVtnxUAEgwnA7hy143EhocVPDLjQ73_rww/exec'
 };
